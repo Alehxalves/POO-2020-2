8006ad93b5b8acfe923f1336eba1e396a5a64bca
Alex Alves <alehxalves@alu.ufc.br>
2021-01-12 08:29:07 -0300

    Up

--- Projeto 07 Cinema/Cinema.java
+++ Projeto 07 Cinema/Cinema.java
@@ -9,7 +9,7 @@ public class Cinema{
         this.cliente = new ArrayList<Cliente>();
 
         for(int i = 0; i < size; i++){
-            cliente.add(i, new Cliente(null, null));
+            cliente.add(null);
         }    
     }
 
@@ -18,7 +18,7 @@ public class Cinema{
             System.out.println("Assento indisponível.");
             return;
         }
-        if(cliente.get(indice).getId() == null){
+        if(cliente.get(indice) == null){
             cliente.set(indice, new Cliente(id + ":", fone));
         }else
             System.out.println("Este assento já está ocupado.");
@@ -30,9 +30,9 @@ public class Cinema{
             System.out.println("Assento indisponível.");
             return false;
         }
-        if(cliente.get(indice).getId() != null){
+        if(cliente.get(indice) != null){
             System.out.println("Reserva cancelada.");
-            cliente.set(indice, new Cliente(null, null));
+            cliente.set(indice ,null);
             return true;
         }else
             System.out.println("Assento já está vago, não foi possível cancelar.");
@@ -42,7 +42,7 @@ public class Cinema{
     public String toString(){  
         String saida = "[ ";
         for(int i = 0; i < cliente.size(); i++){
-            if(this.cliente.get(i).getId() == null){
+            if(this.cliente.get(i) == null){
                 saida += "- ";
             }else
                 saida += cliente.get(i).getId()+ cliente.get(i).getFone() + " ";
